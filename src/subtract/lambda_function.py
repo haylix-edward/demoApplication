9f7849c1a47618964250c7c31896663a1c44a1a2
Edward Rousseau <edward.rousseau@haylix.com>
2018-08-31 15:57:42 +1000

    new subtract function

---
+++ src/subtract/lambda_function.py
@@ -0,0 +1,23 @@
+import json
+
+def lambda_handler(event, context):
+    try:
+        body = {
+            "answer": event["a"] - event["b"]
+        }
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type" : "application/json"
+            },
+            "body": body
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type" : "application/json"
+            },
+            "body": json.dumps(e.message)
+        }
+
